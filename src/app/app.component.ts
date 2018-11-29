7e2c4d3fb5e8dc5bf605f1c02de9e7e5898eb04f
Reina <renamoo808@gmail.com>
2018-11-29 23:58:13 +0900

    initial commits

---
+++ src/app/app.component.ts
@@ -0,0 +1,58 @@
+import { Component, OnInit } from '@angular/core';
+import { AsyncSubject, BehaviorSubject, ReplaySubject, Subject } from 'rxjs';
+
+export enum Subjects {
+  Subject = 'Subject',
+  Behavior = 'BehaviorSubject',
+  Replay = 'ReplaySubject',
+  Async = 'AsyncSubject'
+}
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit {
+  subs: Map<
+    string,
+    | Subject<string>
+    | BehaviorSubject<string>
+    | ReplaySubject<string>
+    | AsyncSubject<string>
+  > = new Map();
+  subNames: string[] = [
+    Subjects.Subject,
+    Subjects.Behavior,
+    Subjects.Replay,
+    Subjects.Async
+  ];
+  value: number = 1;
+  buffer: number = 3;
+
+  ngOnInit() {
+    this.subNames.forEach(sub => {
+      this.subs.set(sub, this.getSubject(sub));
+    });
+  }
+
+  private getSubject(name: string) {
+    switch (name) {
+      case Subjects.Subject:
+        return new Subject<string>();
+      case Subjects.Behavior:
+        return new BehaviorSubject<string>('0');
+      case Subjects.Replay:
+        return new ReplaySubject<string>(this.buffer);
+      case Subjects.Async:
+        return new AsyncSubject<string>();
+    }
+  }
+
+  onNext() {
+    this.subNames.forEach(name => {
+      this.subs.get(name).next(`${this.value}`);
+    });
+    this.value++;
+  }
+}
---
+++ src/app/block/block.component.ts
@@ -0,0 +1,34 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { AsyncSubject, BehaviorSubject, ReplaySubject, Subject } from 'rxjs';
+
+@Component({
+  selector: 'app-block',
+  templateUrl: './block.component.html',
+  styleUrls: ['./block.component.scss']
+})
+export class BlockComponent implements OnInit {
+  @Input() sub:
+    | Subject<string>
+    | BehaviorSubject<string>
+    | ReplaySubject<string>
+    | AsyncSubject<string>;
+  @Input() name: string;
+  isReceived: boolean = false;
+  currentVal: string;
+
+  constructor() {}
+
+  ngOnInit() {
+    this.subscribe();
+  }
+
+  subscribe() {
+    this.sub.subscribe(val => {
+      this.currentVal = val;
+      this.isReceived = true;
+      setTimeout(() => {
+        this.isReceived = false;
+      }, 3000);
+    });
+  }
+}
