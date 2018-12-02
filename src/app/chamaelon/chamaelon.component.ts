7df98505323c08fd4b3406e298737a02e4f362b1
Reina <renamoo808@gmail.com>
2018-12-03 00:00:34 +0900

    chameleon base

---
+++ src/app/chamaelon/chamaelon.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-chamaelon',
+  templateUrl: './chamaelon.component.html',
+  styleUrls: ['./chamaelon.component.css']
+})
+export class ChamaelonComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
