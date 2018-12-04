c8973202e4a3a18566fc83753eb9e2efc9f81a7d
Reina <renamoo808@gmail.com>
2018-12-04 23:13:23 +0900

    refs #0 add chame and complete

--- src/app/chamaelon/chamaelon.component.ts
+++ src/app/chamaelon/chamaelon.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-chamaelon',
   templateUrl: './chamaelon.component.html',
   styleUrls: ['./chamaelon.component.css']
 })
-export class ChamaelonComponent implements OnInit {
+export class ChamaelonComponent implements OnChanges {
+  @Input() length: number = 640;
+  @Input() bodyColor: string = 'orange';
+  viewBox = '0 0 640 640';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  ngOnChanges(sc: SimpleChanges) {
+    if (sc['Input']) {
+      this.viewBox = `0 0 ${length} ${length}`;
+    }
   }
-
 }
